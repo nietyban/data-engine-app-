f49150f65dc76e8ff2143b2c00c89b24847d9707
nietyban <nietyban@amazon.com>
2026-05-07 10:33:09 -0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '40px', fontFamily: 'system-ui' }}>
+      <h1>Data Engine Scheduler</h1>
+      <p>App is live. Full scheduling interface coming soon.</p>
+    </div>
+  )
+}
